d6d2d575983997c5ecff7ec7254b5247e0cc2d21
Andrew Shopland <hi@ashp.land>
2017-10-24 11:47:55 -0700

    Start Cat Map

---
+++ Cats/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  Cats
+//
+//  Created by Andrew on 2017-10-24.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import MapKit;
+
+@interface MapViewController : UIViewController <MKMapViewDelegate>
+
+@end
---
+++ Cats/MapViewController.m
@@ -0,0 +1,22 @@
+//
+//  MapViewController.m
+//  Cats
+//
+//  Created by Andrew on 2017-10-24.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
